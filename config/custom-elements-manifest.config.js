8d4e5b2ced6696404d547089543862d7ac2d1b2d
ethangardner <ethan.gardner@gsa.gov>
2025-11-19 11:19:32 -0500

    add react wrapper for components

--- config/custom-elements-manifest.config.js
+++ config/custom-elements-manifest.config.js
@@ -1,4 +1,13 @@
+import { customElementReactWrapperPlugin } from "custom-element-react-wrappers";
+
 export default {
+  plugins: [
+    customElementReactWrapperPlugin({
+      outdir: './dist/components/frameworks/react',
+      modulePath: (_, tagName) =>
+        `../../${tagName}.js`,
+    }),
+  ],
   globs: ["./src/components/**/*.{js,ts}"],
   exclude: [
     "./src/components/**/*.spec.{js,ts}",
