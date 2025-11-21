bd511dd650203cc6f4694b4efc28c2ac2a07798a
ethangardner <ethan.gardner@gsa.gov>
2025-11-21 14:04:25 -0500

    add react example

---
+++ examples/with-react/src/App.tsx
@@ -0,0 +1,20 @@
+import "./App.css";
+/**
+ * USWDS Elements provides a React wrapper of each component.
+ * Import the components you need as shown in the example the below.
+ * For purposes of demonstration, we are importing the USA Banner component.
+ */
+import { UsaBanner } from "@uswds/elements/components/frameworks/react/index";
+
+function App() {
+  return (
+    <>
+      {/* With the React wrapper components, you import and use USWDS Elements as you would any other React component */}
+      <UsaBanner />
+      <h1>My React App</h1>
+      {/* the rest of your app... */}
+    </>
+  );
+}
+
+export default App;
