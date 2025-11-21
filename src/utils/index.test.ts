d32b104bc1bcf6fb406d8ed08c9bd93b046c5465
ethangardner <ethan.gardner@gsa.gov>
2025-11-21 16:01:01 -0500

    simplify test

--- src/utils/index.test.ts
+++ src/utils/index.test.ts
@@ -4,37 +4,31 @@ import { defineCustomElement } from "./index";
 describe("defineCustomElement", () => {
   let customElementsDefineSpy: ReturnType<typeof vi.spyOn>;
   let customElementsGetSpy: ReturnType<typeof vi.spyOn>;
+  let Component;
+  let tagName: string;
 
   beforeEach(() => {
-    vi.restoreAllMocks();
+    Component = class extends HTMLElement {};
+    tagName = "usa-test-element";
 
     customElementsDefineSpy = vi.spyOn(customElements, "define");
     customElementsGetSpy = vi.spyOn(customElements, "get");
   });
 
   it("should define a custom element if it does not already exist", () => {
-    customElementsGetSpy.mockReturnValue(undefined); // Simulate element not defined
-
-    class MyElement extends HTMLElement {}
-    const tagName = "my-test-element";
-
-    defineCustomElement(tagName, MyElement);
+    defineCustomElement(tagName, Component);
 
     expect(customElementsGetSpy).toHaveBeenCalledWith(tagName);
     expect(customElementsDefineSpy).toHaveBeenCalledTimes(1);
-    expect(customElementsDefineSpy).toHaveBeenCalledWith(tagName, MyElement);
+    expect(customElementsDefineSpy).toHaveBeenCalledWith(tagName, Component);
   });
 
   it("should not define a custom element if it already exists", () => {
-    class ExistingElement extends HTMLElement {}
-    customElementsGetSpy.mockReturnValue(ExistingElement); // Simulate element already defined
-
-    class MyElement extends HTMLElement {}
-    const tagName = "my-existing-element";
-
-    defineCustomElement(tagName, MyElement);
+    defineCustomElement(tagName, Component);
+    defineCustomElement(tagName, Component);
 
     expect(customElementsGetSpy).toHaveBeenCalledWith(tagName);
-    expect(customElementsDefineSpy).not.toHaveBeenCalled();
+    // we still expect this to be called once because of the conditional check
+    expect(customElementsDefineSpy).toHaveBeenCalledTimes(1);
   });
 });
