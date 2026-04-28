74e2915d99e46080afb42ba7d62ae67be7c04e7d
Matt Henry <mattheww.henry@maryland.gov>
2026-04-28 14:11:44 -0400

    Update docs

--- .storybook/UswdsTheme.js
+++ .storybook/UswdsTheme.js
@@ -1,11 +1,11 @@
 import { create } from "storybook/theming/create";
 
 export default create({
-  base: "light",
-  brandTitle: "USWDS Web components",
-  fontBase:
-    '"Public Sans Web", -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Helvetica, Arial, sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol"',
-  colorPrimary: "#0050d8",
-  colorSecondary: "#1a4480",
-  textColor: "#1b1b1b",
+    base: "light",
+    brandTitle: "OGDS Elements",
+    fontBase:
+        '"Public Sans Web", -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Helvetica, Arial, sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol"',
+    colorPrimary: "#0050d8",
+    colorSecondary: "#1a4480",
+    textColor: "#1b1b1b",
 });
