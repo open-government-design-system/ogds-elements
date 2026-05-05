aa33cc1631dd9b42a939292881acfd38c3c56b69
Matt Henry <mattheww.henry@maryland.gov>
2026-05-05 15:18:37 -0400

    Add toggle to barrel file

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,4 +1,5 @@
 import { OgdsBanner } from "./ogds-banner";
 import { OgdsAccordion } from "./ogds-accordion";
+import { OgdsAccordionToggle } from "./ogds-accordion-toggle";
 
-export { OgdsBanner, OgdsAccordion };
+export { OgdsBanner, OgdsAccordion, OgdsAccordionToggle };
