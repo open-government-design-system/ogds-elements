923c92bff86facea0fca8ce4d1a7507ce5d1654c
Max Jacobson <maxwell.jacobson@maryland.gov>
2026-07-01 14:04:11 -0400

    Add meta test to ensure playwright versions match

---
+++ src/meta.test.js
@@ -0,0 +1,36 @@
+import { describe, it, expect } from "vitest";
+import { execSync } from "child_process";
+
+describe("meta", () => {
+  function playwrightVersionFromPackageJson() {
+    const output = execSync(
+      "node_modules/.bin/playwright --version",
+    ).toString();
+    const version = output.match(/Version (\d+\.\d+.\d+)/)[1];
+    return version;
+  }
+
+  function findDockerPlaywrightReferences() {
+    const output = execSync(
+      "git ls-files | xargs grep --line-number mcr.microsoft.com/playwright",
+    )
+      .toString()
+      .trim();
+
+    return output.split("\n");
+  }
+
+  it("should reference the same version of Playwright everywhere", () => {
+    const expectedPlaywrightVersion = playwrightVersionFromPackageJson();
+    expect(expectedPlaywrightVersion).toBeTypeOf("string");
+
+    const otherReferences = findDockerPlaywrightReferences();
+
+    for (const reference of otherReferences) {
+      expect(
+        reference,
+        "When updating playwright, make sure to go manually update all of the Docker referencences to use the same version too",
+      ).toContain(expectedPlaywrightVersion);
+    }
+  });
+});
